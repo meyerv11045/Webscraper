3e35c94d724eaf4ea7debbaaf246115e3534bb8c
Vikram Meyer <vjmeyer20@gmail.com>
2020-08-10 14:34:08 -0400

    Updated documentation

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 ## Setup:
 1. `pip install selenium`
 2. Download and install the Chrome browser if not installed
-3. Download the chromedriver and install it in the project's root directory
+3. Download chromedriver (https://chromedriver.chromium.org/downloads) and install it in the project's root directory
+
+If there are problems w/ chromedriver, download geckodriver (for firefox) and change the webdriver in `scrape.py` from Chrome to FireFox
+
+## Input file
+The input csv file should contain two columns: 
+1. The first column should be the upcUuid for the product
+2. The second column should be the url for the product 
+
+To change the nutrition fact scraper to only require a csv file with urls change line 177 in `KrogerNutritionFacts.main()` to `url = line[0]`
+
+## Scraping More Info
+To scrape different information, create a new class that inherits from the `Scraper` class. This will allow you to modify what is scraped and how. Xpaths to elements can be found in the inspection panel and by clicking an element and selecting `Copy full xpath`. The scraper can be run in headless mode by adding the following but some websites cannot be scraped in headless mode (e.g. kroger.com)
+```python
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
+```
--- scrape.py
+++ scrape.py
@@ -7,17 +7,22 @@ from selenium.common.exceptions import TimeoutException,NoSuchElementException,W
 import csv
 import re
 
-class NutritionFactsScraper:
+class Scraper:
     def __init__(self,input_file,output_file):
         self.input_file = input_file
         self.output_file = output_file
 
     def init_webdriver(self,try_again=0):
+        """ Initializes and returns a new webdriver object. If there are errors
+            it recursively trys again for 5 trys.
+        Args:
+            try_again (int, optional): Used by function for recursively trying again. Defaults to 0.
+
+        Returns:
+            webdriver object: Chrome webdriver operating in headful mode (headless doesn't work w/ kroger)
+        """
         try:
-            #options = Options()
-            #options.headless = True
             driver = webdriver.Chrome(executable_path='./chromedriver')
-            #driver = webdriver.Firefox(executable_path='./geckodriver',options=options)
             return driver
         except WebDriverException:
             print('Connection Refused ... Trying Again')
@@ -25,10 +30,20 @@ class NutritionFactsScraper:
                 return self.init_webdriver(try_again= (try_again + 1))
             else: 
                 return None
+
+class KrogerNutritionFacts(Scraper):
+    def __init__(self,input_file,output_file):
+        super().__init__(input_file,output_file)
+
     @staticmethod
     def click_confirm_location(driver):
+        """ Clicks Confirm Location button that is overlayed
+            so that the other buttons on the page work
+
+        Args:
+            driver (webdriver object): On kroger product landing page 
+        """
         try:
-            #Confirm the location so other buttons work
             confirm_xpath = '/html/body/div[1]/div[2]/div[2]/div[3]/div/div/div[6]/button[1]'
             confirm_loc_btn = WebDriverWait(driver,6).until(
                 EC.presence_of_element_located((By.XPATH,confirm_xpath))
@@ -37,8 +52,18 @@ class NutritionFactsScraper:
             print('Location Confirmed')
         except TimeoutException:
             print('No Confirmation Button')
+
     @staticmethod
     def get_nutrition_from_xpaths(driver,calories_xpath,fat_xpath,carbs_xpath,protein_xpath):
+        """Collects nutrition information from xpaths
+
+        Args:
+            driver (webdriver object): On kroger product page 
+            *_xpath (str): xpaths to the different nutrition elements
+
+        Returns:
+            list: the collected nutrition info from the xpaths
+        """
         calories = driver.find_element_by_xpath(calories_xpath).text
         fat = driver.find_element_by_xpath(fat_xpath).text
         carbs = driver.find_element_by_xpath(carbs_xpath).text
@@ -48,7 +73,12 @@ class NutritionFactsScraper:
     
     @staticmethod
     def click_nutrition_button(driver):
-        #Select the nutrition view to render the nutrition info
+        """ Clicks the nutrition button to view the nutrition info
+            on a kroger product page
+
+        Args:
+            driver (webdriver object): On kroger product page w/ confirm location btn clicked
+        """
         nutrition_xpath = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/div/button[2]'
         nutritionBtn = WebDriverWait(driver,2).until(
             EC.presence_of_element_located((By.XPATH,nutrition_xpath))
@@ -56,7 +86,17 @@ class NutritionFactsScraper:
         nutritionBtn.click()
         print('Nutrition Btn Clicked')
 
-    def scrape_nutrition(self,url,try_again=0):
+    def scrape_nutrition(self,url):
+        """ Scrapes nutrition info from webpage using multiple possible
+            xpaths for the nutrition infos location and returns the collected
+            nutrition information as a dictionary
+
+        Args:
+            url (str): Kroger product url
+
+        Returns:
+            dict: Contains collected calorie, fat, carb, and protein information
+        """
         info = {
             'calories': None,
             'fat': None,
@@ -67,31 +107,31 @@ class NutritionFactsScraper:
         if driver is None: return info
         try:
             driver.get(url)
-            NutritionFactsScraper.click_confirm_location(driver)
+            KrogerNutritionFacts.click_confirm_location(driver)
             try:
                 #Nutrition displayed on main view (No buttons to click)
                 calories_xpath1 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section/div/div/div[2]/div[1]/div/div[5]/span[2]'
                 fat_xpath1 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section/div/div/div[2]/div[1]/div/div[7]/div[1]/span[1]'
                 carbs_xpath1 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section/div/div/div[2]/div[1]/div/div[10]/div[1]/span[1]'
                 protein_xpath1 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section/div/div/div[2]/div[1]/div/div[11]/div[1]/span[1]'
-                calories, fat, carbs, protein = NutritionFactsScraper.get_nutrition_from_xpaths(driver,calories_xpath1,fat_xpath1,carbs_xpath1,protein_xpath1)
+                calories, fat, carbs, protein = KrogerNutritionFacts.get_nutrition_from_xpaths(driver,calories_xpath1,fat_xpath1,carbs_xpath1,protein_xpath1)
             except (TimeoutException, NoSuchElementException):
                 #Nutrition Button to view Nutrition Info
                 try: 
-                    NutritionFactsScraper.click_nutrition_button(driver)
+                    KrogerNutritionFacts.click_nutrition_button(driver)
                     try: #Nutrition Btn Section 2 in xpath
                         calories_xpath2 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section[2]/div/div/div[2]/div[1]/div/div[5]/span[2]'             
                         fat_xpath2 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section[2]/div/div/div[2]/div[1]/div/div[7]/div[1]/span[1]' 
                         carbs_xpath2 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section[2]/div/div/div[2]/div[1]/div/div[10]/div[1]/span[1]'
                         protein_xpath2 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section[2]/div/div/div[2]/div[1]/div/div[11]/div[1]/span[1]'
-                        calories, fat, carbs, protein =  NutritionFactsScraper.get_nutrition_from_xpaths(driver,calories_xpath2,fat_xpath2,carbs_xpath2,protein_xpath2)
+                        calories, fat, carbs, protein =  KrogerNutritionFacts.get_nutrition_from_xpaths(driver,calories_xpath2,fat_xpath2,carbs_xpath2,protein_xpath2)
                     except (TimeoutException,NoSuchElementException):
                         try: #Nutrition Btn Section 1 in xpath
                             calories_xpath3 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section[1]/div/div/div[2]/div[1]/div/div[5]/span[2]'
                             fat_xpath3 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section[1]/div/div/div[2]/div[1]/div/div[7]/div[1]/span[1]'
                             carbs_xpath3 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section[1]/div/div/div[2]/div[1]/div/div[10]/div[1]/span[1]'
                             protein_xpath3 = '/html/body/div[1]/div[2]/div[2]/div[1]/main/div/div/div[3]/section[1]/div/div/div[2]/div[1]/div/div[11]/div[1]/span[1]'
-                            calories, fat, carbs, protein = NutritionFactsScraper.get_nutrition_from_xpaths(driver,calories_xpath3,fat_xpath3,carbs_xpath3,protein_xpath3)
+                            calories, fat, carbs, protein = KrogerNutritionFacts.get_nutrition_from_xpaths(driver,calories_xpath3,fat_xpath3,carbs_xpath3,protein_xpath3)
                         except (TimeoutException,NoSuchElementException):
                             print('No Nutrition Info')
                             return info
@@ -125,13 +165,16 @@ class NutritionFactsScraper:
         finally:
             driver.quit() 
     
-    def main(self): 
+    def main(self):
+        """ Iterates through the upcUuids and urls provided in the input csv file,
+            scrapes the nutrition information, and writes it to a csv file
+        """ 
         with open(self.input_file,'r') as f1, open(self.output_file,'w') as f2:
             reader, writer = csv.reader(f1), csv.writer(f2)
             next(reader)
             writer.writerow(['upcUuid','krogerUrl','fat','fatUnits','carbs','carbsUnits','protein','proteinUnits','calories','caloriesUnits'])
             for val,line in enumerate(reader,1):
-                upcUuid, url = line[0],line[1]
+                upcUuid, url = line[0],line[1] #upcUuid should be 1st col of input file and url shoudld be 2nd col 
                 info = self.scrape_nutrition(url)
                 print(info)
                 writer.writerow([upcUuid,url,info['fat'],'g',info['carbs'],'g',info['protein'],'g',info['calories'],'cal'])
@@ -140,5 +183,5 @@ class NutritionFactsScraper:
 if __name__ == '__main__':
     input_file = input('Enter path to input csv file: ') 
     output_file = input('Enter path to output csv file')
-    scraper = NutritionFactsScraper(input_file,output_file)
+    scraper = KrogerNutritionFacts(input_file,output_file)
     scraper.main()
